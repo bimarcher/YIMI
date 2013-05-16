17f04008ea6fe4a89dec3d6da866be514c5409c8
bimarcher <x_che@126.com>
2013-05-16 17:15:00 +0800

    尝试新的方法

--- DynamicEntityCreator/src/com/yimi/archer/base/ClassBuilder.java
+++ DynamicEntityCreator/src/com/yimi/archer/base/ClassBuilder.java
@@ -1,16 +1,36 @@
 package com.yimi.archer.base;
 
-import java.util.Collection;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.URL;
 
 public class ClassBuilder {
-	public Class build(String _className, Collection properties) {
+	private static URL m_classpath_url = ClassLoader.getSystemResource("");
 
-		Class cls = null;
-		try {
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+	public static Class build(String _className) throws IOException {
+
+		String sourceFilePath = m_classpath_url.getFile() + _className + ".java";
+		// File sourceFile = new File(sourceFilePath);
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(sourceFilePath)));
+		bw.write("something");
+
+		// Runtime runtime = Runtime.getRuntime();
+		// try {
+		// runtime.exec("javac e:\\" + _className);
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
 		return null;
+	}
 
+	public static void main(String[] args) throws IOException {
+		System.out.println(ClassBuilder.m_classpath_url);
+		ClassBuilder.build("Hello");
 	}
 }
